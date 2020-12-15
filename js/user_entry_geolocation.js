2608bc97f42dbc75afeff05ade5b42aa9141656f
HTOO MAUNG THAIT <htoo.mt@blueplanet.com.mm>
2020-12-15 23:39:13 +0630

    save location finish

--- js/user_entry_geolocation.js
+++ js/user_entry_geolocation.js
@@ -1,7 +1,10 @@
 let mapPinLocation;
 let zoomLevel;
 let getCurrentLocation;
+let currentLocation;
+let currentLocationMarker;
 let map;
+let markers = [];
 
 setLocation({ lat: 16.8409, lng: 96.1735 });
 setZoomLevel(12);
@@ -9,6 +12,10 @@ setZoomLevel(12);
 function initMap(){
     let center = this.mapPinLocation;
     let zoom = this.zoomLevel;
+    let txtUserLocation = document.getElementById("txt_user_location");
+    let jsonCurrentLat = document.getElementById("json_current_lat");
+    let jsonCurrentLng = document.getElementById("json_current_lng");
+
 
     let options = {
         center : center,
@@ -16,9 +23,72 @@ function initMap(){
     }
     map = new google.maps.Map(document.getElementById('map'), options);
 
+
+    //get current location and map settings
     getCurrentLocation = () => {
-        alert("Hello this is your location");
+        if(navigator.geolocation){
+            navigator.geolocation.getCurrentPosition(
+                (position )=> {
+                    let pos = {
+                        lat : position.coords.latitude,
+                        lng : position.coords.longitude,
+                    };
+                    // setCurrentLocation(pos);
+                    // setLocation(pos);
+                    currentLocation = pos;
+                    let posStr = pos.lat + "," + pos.lng;
+                    txtUserLocation.value = posStr;
+                    currentLocationMarker = addMarker({coords : currentLocation, label : "Current Position"});
+                    jsonCurrentLat.value = position.coords.latitude;
+                    jsonCurrentLng.value = position.coords.longitude;
+
+                    map.setCenter(currentLocation);
+                    map.setZoom(17)
+
+
+                },
+                ()=> {
+                    handleLocationError(true, txtUserLocation, map.getCenter());
+                }
+            )
+        }
     }
+
+    function addMarker(props) {
+
+        var marker = new google.maps.Marker({
+            position: props.coords,
+            map: map,
+            draggable:true
+
+        });
+
+        //Check if lablel
+        if (props.label) {
+            marker.setLabel(props.label);
+        }
+
+        //Check if for custom
+        if (props.icon) {
+            //set icon
+            marker.setIcon(props.icon);
+        }
+
+        //Check content
+        if (props.content) {
+            marker.addListener('click', function () {
+                //added marker listener event
+
+            });
+
+        }
+        markers.push(marker);
+        return marker;
+    }
+
+
+
+
 
 }
 
@@ -27,10 +97,47 @@ function setLocation(location){
     this.mapPinLocation = location;
 }
 
+function setCurrentLocation(location){
+    this.currentLocation = location;
+}
+
 function setZoomLevel(zoomLvl){
     this.zoomLevel = zoomLvl;
 }
 
 $(document).ready(function(){
-     $("#get_current_location").click(getCurrentLocation);
+     $("#get_current_location").click(function(){
+         getCurrentLocation();
+
+     });
+
+     $("#btnSaveLocation").click(function(){
+         let currentLat = $("#json_current_lat").val();
+         let currentLng = $("#json_current_lng").val();
+         currentLocation = { lat : currentLat, lng : currentLng};
+         setLocation(currentLocation);
+         // console.log(currentLocation);
+
+         // let defaultLocation = require('../storage/user_entry_geolocation.json');
+         // console.log(defaultLocation);
+         let currentURL = window.location.href;
+         let currentHostSegment = currentURL.replace("user_entry_geolocation.html?#", "");
+         let phpSetUserLocationURL = currentHostSegment + "php/user_entry_location.php?action=setLocation";
+         // console.log(phpSetUserLocationURL);
+
+         $.ajax({
+             url : phpSetUserLocationURL,
+             method : 'POST',
+             header : "Content-Type: application/json",
+             data : currentLocation,
+             success : function(res){
+                 console.log(res);
+             },
+             error : function(res){
+                 console.log(res);
+             }
+         })
+
+
+     });
 });
---
+++ php/user_entry_location.php
@@ -0,0 +1,41 @@
+<?php
+$fileLocation = __DIR__."/../storage/user_entry_geolocation.json";
+
+if(isset($_REQUEST['action']) && $_REQUEST['action'] == 'setLocation'){
+    $currentLat = $_POST['lat'];
+    $currentLng = $_POST['lng'];
+
+
+
+
+    $contentToPut = [
+        "lat" => $currentLat,
+        "lng" => $currentLng
+    ];
+
+    $jsonContentToPut = json_encode($contentToPut);
+    file_put_contents($fileLocation,"");
+    file_put_contents($fileLocation,$jsonContentToPut);
+    fclose($fileLocation);
+    $location = file_get_contents($fileLocation);
+    $location = json_decode($location, JSON_UNESCAPED_SLASHES);
+
+
+    header("Content-Type:application/json; charset:UTF-8;");
+    echo json_encode(["status" => "ok", "location" => $location], JSON_PRETTY_PRINT );
+}
+
+if(isset($_REQUEST['action']) && $_REQUEST['action'] == 'getLocation'){
+    $location = file_get_contents($fileLocation);
+    $location = json_decode($location, JSON_UNESCAPED_SLASHES);
+
+
+    header("Content-Type:application/json; charset:UTF-8;");
+    echo json_encode(["status" => "ok", "location" => $location], JSON_PRETTY_PRINT );
+}
+
+
+
+
+
+
